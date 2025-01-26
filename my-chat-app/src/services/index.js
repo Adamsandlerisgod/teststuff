bcd1c8b989b4972aa51e67baddc707df9570347b
Adamsandlerisgod <wolfroberthendrikse2000@gmail.com>
2025-01-26 17:50:33 +0700

    transcendence testapp

---
+++ my-chat-app/src/services/index.js
@@ -0,0 +1,4 @@
+import { fetchMessages, fetchOnlineUsers, blockUser, inviteToPlay } from './chatService.js'; // Named imports
+import  socket from './socket.js'; // Named import
+
+export {fetchMessages, fetchOnlineUsers, blockUser, inviteToPlay, socket}
