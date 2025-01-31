856b39a858a1f2a59fc7f31ef8a870acee76fb26
Wolf Hendrikse <wolfroberthendrikse2000@gmail.com>
2025-01-31 12:57:56 +0700

    "Deleting Useless stuff"

--- my-chat-app/src/components/index.js
+++
@@ -1,5 +0,0 @@
-import ChatWindow from './ChatWindow/ChatWindow.vue';
-import Message from './Message/Message.vue';
-import UserList from './UserList/UserList.vue';
-
-export {ChatWindow, UserList}
--- my-chat-app/src/services/index.js
+++
@@ -1,4 +0,0 @@
-import { fetchMessages, fetchOnlineUsers, blockUser, inviteToPlay } from './chatService.js'; // Named imports
-import  socket from './socket.js'; // Named import
-
-export {fetchMessages, fetchOnlineUsers, blockUser, inviteToPlay, socket}
